311f2f922defdcf2ad24b93b05e946ece31236e7
Midry <115441271+midry3125@users.noreply.github.com>
2026-01-22 20:25:33 +0900

    整理

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
-#/bin/sh
+#!/bin/sh
 
-if [[ $1 = "ains" ]]; then
-  cp ./ains/* ~/
-elif [[ $1 = "hato" ]]; then
-  cp ./hato.yml ~/
+if [ "$1" = "ains" ]; then
+  cp ./ains/.bashrc ~/
+  echo done.
+elif [ "$1" = "tools" ]; then
+  cp ./tools/hato.yml ~/
+  #cp ./tools/.*  ~/
+  echo done.
+elif [ "$1" = "wsl" ]; then
+  cp ./wsl/.bashrc ~/
+  echo done.
 fi
